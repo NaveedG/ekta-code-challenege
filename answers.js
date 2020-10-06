5d63243b6b76a972c0cc850e900c2a47a072a085
NaveedG <nganjani@protonmail.com>
2020-10-06 14:57:54 -0500

    questions 2 and 3 completed

--- answers.js
+++ answers.js
@@ -1,7 +1,7 @@
 // #1 What is this code doing? How might it be improved?
 return !this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.subThreadName ) .filter(q => q.mrLevel == this.currentQuestion.mrLevel) .some( q => q.currentAnswer == "No")
 
-/* This code is performing the following operations:
+/* Answer: This code is performing the following operations:
 
 1.) Iterating through an array of questions (an instance of allQuestions) to remove any question which has a subThreadName that is not equal in value to the current question's subThreadName.
 2.) Iterating through the array of remaining questions from (1) to remove any question which has a mrLevel that is not equal in value to the current question's mrLevel.
@@ -11,3 +11,23 @@ return !this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.su
 The clarity of the code may be improved by removing the 'not' operator at the beginning and modifying the third iteration: */
 
 return this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.subThreadName ).filter(q => q.mrLevel == this.currentQuestion.mrLevel).every( q => q.currentAnswer != "No")
+
+
+
+// #2 What is this code doing?
+var oldAssessment = this.allQuestions.map( q => Object.assign({}, q));
+
+/* Answer: This code is assigning the variable oldAssessment (using the older and more flexible var keyword) to a new array of objects which are just copies of all of the questions from the allQuestions array.
+During each iteration, the first argument passed into Object.assign will create an empty object, and the second argument will overwrite the empty object with the content of a question from the allQuestions array. */
+
+
+
+// #3 What is this code doing?
+const old = this.allQuestions.map(function(question: any) { return {...question}; })
+
+/* Answer: Similar to the previous question, this code is assigning the variable old (using the newer and less flexible const keyword) to a new array of objects which are just copies of all of the questions from the allQuestions array.
+During each iteration, the callback function passed into the map function will return a new object containing the content of a question from the allQuestions array by using the spread operator. */
+
+
+
+
