ff58581d8b3ea11a139658f71b420f979d465f52
NaveedG <nganjani@protonmail.com>
2020-10-06 15:44:38 -0500

    final draft

--- answers.js
+++ answers.js
@@ -4,11 +4,11 @@ return !this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.su
 /* Answer: This code is performing the following operations:
 
 1.) Iterating through an array of questions (an instance of allQuestions) to remove any question which has a subThreadName that is not equal in value to the current question's subThreadName.
-2.) Iterating through the array of remaining questions from (1) to remove any question which has a mrLevel that is not equal in value to the current question's mrLevel.
-3.) Iterating through the array of remaining questions from (2) to check if any of the questions have a current answer with the value of "No" and therefore return true.
+2.) Iterating through the array of remaining questions from step (1) to remove any question which has a mrLevel that is not equal in value to the current question's mrLevel.
+3.) Iterating through the array of remaining questions from step (2) to check if any of the questions have a current answer with the value of "No" and therefore return true.
 4.) If the final iteration returns true, then false will be returned due to the logical operator 'not' (!) at the beginning. If the final iteration returns false, then true will be returned.
 
-The clarity of the code may be improved by removing the 'not' operator at the beginning and modifying the third iteration: */
+The clarity of the code may be improved by removing the 'not' operator at the beginning, which requires us to modify the third iteration: */
 
 return this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.subThreadName ).filter(q => q.mrLevel == this.currentQuestion.mrLevel).every( q => q.currentAnswer != "No")
 
@@ -17,22 +17,20 @@ return this.allQuestions.filter(q => q.subThreadName == this.currentQuestion.sub
 // #2 What is this code doing?
 var oldAssessment = this.allQuestions.map( q => Object.assign({}, q));
 
-/* Answer: This code is assigning the variable oldAssessment (using the older and more flexible var keyword) to a new array of objects which are just copies of all of the questions from the allQuestions array.
-During each iteration, the first argument passed into Object.assign will create an empty object, and the second argument will overwrite the empty object with the content of a question from the allQuestions array. */
+/* Answer: The variable oldAssessment (using the older and more flexible var keyword) is assigned to a new array of objects which are just copies of all of the questions from the allQuestions array.
+During each iteration, the first argument passed into Object.assign will create an empty object. The second argument will overwrite the empty object with the contents of a question from the allQuestions array. */
 
 
 
 // #3 What is this code doing?
 const old = this.allQuestions.map(function(question: any) { return {...question}; })
 
-/* Answer: Similar to the previous question, this code is assigning the variable old (using the newer and less flexible const keyword) to a new array of objects which are just copies of all of the questions from the allQuestions array.
-During each iteration, the callback function passed into the map function will return a new object containing the content of a question from the allQuestions array by using the spread operator. */
+/* Answer: Similar to the previous code, variable old (using the newer and less flexible const keyword) is assigned to a new array of objects which are just copies of all of the questions from the allQuestions array.
+During each iteration, the callback function passed into the map function will return a new object containing the contents of a question from the allQuestions array by using the spread operator. */
 
 
 
 // #4 I have an array of objects, they are currently untyped. I'm unsure of how but mixed in with my normal objects ({ id, name, date, createdAt }), are null values. How would you eliminate the null values?
-
-let arr = [id, name, date, createdAt, nullVal]
 
 removeNull = arr => {
   return arr.filter(obj => obj != null)
@@ -57,12 +55,8 @@ headerMessage: string;
   // HERE is where I want a header message
 </div>``` */
 
-/* Answer: A variable may be passed from a parent component to a child component through property binding:
-1.) Import the Input decorator into the child component (i.e., header-component) TS file
-2.) Bind the parent component's property (i.e., headerMessage) to the child component using the @Input decorator
-3.) The binded property may then be displayed in the child component's HTML template
-*/
+/* Answer: A variable may be passed from a parent component to a child component through property binding, which involves the following steps:
 
-
-
-
+1.) Bind the parent component's property (i.e, headerMessage) to a child component's property in the parent's HTML template where the child component will be displayed.
+2.) Import the Input decorator itself into the child component (i.e., header-component) TS file.
+3.) Add the binded property to the child component TS file by preceded by the @Input decorator. */
